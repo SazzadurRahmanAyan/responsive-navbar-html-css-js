7b8b6dd254a001fa1ab917ba07c5c41bd9a6eb7a
SazzadurRahmanAyan <sazzadurrahman.ayan@outlook.com>
2023-12-03 20:00:33 +0600

    search bar added

--- main.js
+++ main.js
@@ -16,3 +16,22 @@ navToggle.addEventListener('click', () => {
 navClose.addEventListener('click', () => {
     navMenu.classList.remove('show-menu')
 })
+
+// Search
+
+const search = document.getElementById('search'),
+      searchBtn = document.getElementById('search-btn'),
+      searchClose = document.getElementById('search-close')
+
+
+// Search show
+
+searchBtn.addEventListener('click', () => {
+    search.classList.add('show-search')
+})
+
+// Search hidden
+
+searchClose.addEventListener('click', () => {
+    search.classList.remove('show-search')
+})
